a30e53390975d7c6aca8861eea179705cb1b7620
Skoh <101289702+SkohTV@users.noreply.github.com>
2023-04-21 20:58:02 +0200

    atomic

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const test = require("./api/test");
 
-app.use(express.json({extented: false}));
+app.use(express.json());
 app.use("/api/test", test);
 
 const PORT = 8080;
