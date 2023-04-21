cb39524b26c2a13d1fe0929d2ad45ed6c1733fe4
Skoh <101289702+SkohTV@users.noreply.github.com>
2023-04-21 20:16:13 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-import express from 'express';
-import path from 'path';
+//import express from 'express';
+//import path from 'path';
 
 
-const app = express();
+//const app = express();
 
-app.get('/', (req,res) => {
-	//res.sendFile(path.join('client', 'pages', 'index.html'))
-	res.send('Hello');
-	console.log('Here');
-})
+//app.get('/', (req,res) => {
+//	//res.sendFile(path.join('client', 'pages', 'index.html'))
+//	res.send('Hello');
+//	console.log('Here');
+//})
 
-app.listen(3000, () => {
-	console.log('Server started on port 3000');
-});
+//app.listen(3000, () => {
+//	console.log('Server started on port 3000');
+//});
