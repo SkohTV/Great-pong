254d6f96ff8427ff59b41f5a67f4f2de24422d40
Skoh <101289702+SkohTV@users.noreply.github.com>
2023-04-21 21:07:30 +0200

    atomic

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ app.use(express.static(__dirname));
 
 // Set up a route for the root URL
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/pages/index.html'));
-  res.send("Yo");
+	res.sendFile(path.join(__dirname, 'client/pages/'));
+	res.send("Yo");
 });
 
 // Start the server
 app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+	console.log('Server is running on port 3000');
 });
