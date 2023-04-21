960cb7134c87a7833c109d7fc156514a83f5d088
Skoh <101289702+SkohTV@users.noreply.github.com>
2023-04-21 21:23:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 // Where our static files are located
-app.use(express.static(__dirname, 'client'));
+app.use(express.static(path.join(__dirname, 'client')));
 
 
 // Main redirect (static)
