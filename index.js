65ed5012fc0ecef3c421372c4a313b8952470b3e
Skoh <101289702+SkohTV@users.noreply.github.com>
2023-04-21 21:21:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,21 +2,25 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname));
+// Where our static files are located
+app.use(express.static(__dirname, 'client'));
 
+
+// Main redirect (static)
 app.get('/:page', (req, res) => {
-	const page = req.params.page;
+	const page = req.params.page; // Extract the requested page
 	switch (page){
-		case '':
-		case 'local':
-		case 'versus':
-		case 'arena':
-			res.sendFile(path.join(__dirname, `client/pages/${page}`));
+		case '': // index.html -> root
+		case 'local': // local.html -> /local
+		case 'versus': // versus.html -> /versus
+		case 'arena': // arena.html -> /arena
+			res.sendFile(path.join(__dirname, `client/pages/${page}.html`));
 			break;
-		default:
+		default: // Else 404
 			res.sendFile(path.join(__dirname, 'client/pages/404.html'));
 	}
 });
+
 
 // Start the server
 app.listen(3000, () => {
