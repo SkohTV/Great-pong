3fa03ae3cd078dc1d341ccbda7c470e122c69aa4
Skoh <101289702+SkohTV@users.noreply.github.com>
2023-04-21 20:06:23 +0200

    atomic

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-import app from '/api/router.js';
+import express from 'express';
 
 
 
+const app = express();
+
+app.get('/', (req,res) => {
+	res.sendFile('/client/pages')
+	console.log('Here');
+})
 
 app.listen(3000, () => {
 	console.log('Server started on port 3000');
